134ce002f69eb433c1304a4fe0452d46be089e60
chethtra yen <chethtra_yen@rocketmail.com>
2017-04-10 00:15:29 -0400

    poop

--- config/faceConfig.js
+++ config/faceConfig.js
@@ -1,2 +1,4 @@
 let oxford = require("project-oxford");
-let client = new oxford.Client('7fe6ec1e951f4f838c0f5d8e0dc495e1');
+let client = new oxford.Client('7fe6ec1e951f4f838c0f5d8e0dc495e1')
+
+module.exports = client
