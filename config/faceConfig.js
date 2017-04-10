efad40b6a1a4a9581619f0ace1e23c61e0f88876
chethtra yen <chethtra_yen@rocketmail.com>
2017-04-10 00:12:09 -0400

    person group

--- config/faceConfig.js
+++ config/faceConfig.js
@@ -1,2 +1,2 @@
-var oxford = require("project-oxford");
-var client = new oxford.Client('7fe6ec1e951f4f838c0f5d8e0dc495e1');
+let oxford = require("project-oxford");
+let client = new oxford.Client('7fe6ec1e951f4f838c0f5d8e0dc495e1');
