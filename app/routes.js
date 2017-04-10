b94e8aa6718c028d5a3bb698fc11256b0bb87bf0
chethtra yen <chethtra_yen@rocketmail.com>
2017-04-09 21:02:43 -0400

    poop

--- app/routes.js
+++ app/routes.js
@@ -8,15 +8,8 @@ let newUser = new fb.NewUser({machineId: '123', firstName: 'first', lastName: 'l
 newUser.printUser()
 
 function decodeBase64Image(dataString) {
-  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-    response = {};
-
-  if (matches.length !== 3) {
-    return new Error('Invalid input string');
-  }
-
-  response.type = matches[1];
-  response.data = new Buffer(matches[2], 'base64');
+  response.type = 'image/png';
+  response.data = new Buffer(dataString, 'base64');
 
   return response;
 }
@@ -80,10 +73,9 @@ module.exports = function (app,faceUpload) {
       })
   })
 
-  app.post('/saveFile',function (req,res) {
-      let data = 'data:image/png;base64,' + req.body.file
-      var imageBuffer = decodeBase64Image(data);
-      fs.writeFile('test.jpg', imageBuffer.data, function(err) {
+  app.post('/saveFile',function (req,res) { 
+      var imageBuffer = decodeBase64Image(req.body.file);
+      fs.writeFile('test.png', imageBuffer.data, function(err) {
         if(err){
           console.log(err)
         }
