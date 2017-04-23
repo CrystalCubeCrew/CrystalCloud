da2f271a4591b4e1c591445abb3721459e60c7e1
chethtra yen <chethtra_yen@rocketmail.com>
2017-04-23 18:36:40 -0400

    added user id to get user

--- config/musicList.js
+++ config/musicList.js
@@ -2,8 +2,8 @@ let list = {
   "starboy" : 'http://d2xb16qkudqwc7.cloudfront.net/starboy2.wav',
   "classical" : 'http://d2xb16qkudqwc7.cloudfront.net/_High+Quality_+Shadow+of+the+Colossus+OST+08+-+The+Opened+Way_AAC_128k+(mp3cut.net).wav',
   'game music': 'http://d2xb16qkudqwc7.cloudfront.net/Undertale+OST_+090+-+His+Theme_AAC_128k+(mp3cut.net).wav',
-  'naruto opening 5' : 'http://d2xb16qkudqwc7.cloudfront.net/Naruto Opening 5 v2_AAC_128k (mp3cut.net).wav',
-  'rock' : 'http://d2xb16qkudqwc7.cloudfront.net/Queen - Bohemian Rhapsody (Official Video)_AAC_128k (mp3cut.net).wav'
+  'naruto opening 5' : 'http://d2xb16qkudqwc7.cloudfront.net/Naruto%20Opening%205%20v2_AAC_128k%20(mp3cut.net).wav',
+  'rock' : 'http://d2xb16qkudqwc7.cloudfront.net/Queen%20-%20Bohemian%20Rhapsody%20(Official%20Video)_AAC_128k%20(mp3cut.net).wav'
 }
 
 module.exports = list
